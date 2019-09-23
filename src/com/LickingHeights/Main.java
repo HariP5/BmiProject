cf0e88269c122278d4b1c924b1ae5668ebfb01c8
Hari Pokhrel <hari.pokhrel.2020@student.lhschools.org>
2019-09-23 09:54:28 -0400

    Added scanner keyboard

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,20 @@
+package com.LickingHeights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+    //Declare
+
+        Scanner keyboard;
+
+
+
+
+    //Initializing
+
+    keyboard = new Scanner(System.in);
+
+    }
+}
