dfb90ec006f41ec43ace6ae0fb422a4330b4cc74
Hari Pokhrel <hari.pokhrel.2020@student.lhschools.org>
2019-09-23 09:57:01 -0400

    Added 6 variables

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -7,6 +7,15 @@ public class Main {
     public static void main(String[] args) {
     //Declare
 
+    String name;
+    float inches;
+    float pounds;
+    float meters;
+    float kilograms;
+    float total;
+
+
+
         Scanner keyboard;
 
 
