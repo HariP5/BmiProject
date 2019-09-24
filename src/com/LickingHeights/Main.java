0e18203d0cd20c27c9b279097f53c89d6d591d59
Hari Pokhrel <hari.pokhrel.2020@student.lhschools.org>
2019-09-24 09:18:30 -0400

    Started writing down the code

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -7,12 +7,12 @@ public class Main {
     public static void main(String[] args) {
     //Declare
 
-    String name;
-    float inches;
-    float pounds;
-    float meters;
-    float kilograms;
-    float total;
+     String name;
+     int inches;
+     float pounds;
+     float meters;
+     float kilograms;
+     float total;
 
 
 
@@ -24,6 +24,11 @@ public class Main {
     //Initializing
 
     keyboard = new Scanner(System.in);
+
+    System.out.println("What is your name");
+    name = keyboard.nextLine();
+    System.out.println("What is your height in (inches)?");
+    inches = keyboard.nextInt();
 
     }
 }
